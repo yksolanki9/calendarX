d9d1945165a5e226ab3fcbf19ff5b353781313dd
Yash Solanki <yashsolankli1709@gmail.com>
2022-05-07 00:41:50 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./google-auth');
+
+const app = express();
+
+app.use('view engine', 'ejs');
+
+app.use(cookieSession({
+  name: 'google-auth-session',
+  keys: ['key1', 'key2']
+}));
+
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+      next();
+  } else {
+      res.sendStatus(401);
+  }
+}
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({message: "You are not logged in"})
+});
+
+app.get("/failed", (req, res) => {
+  res.send("Failed")
+});
+
+app.get("/success", isLoggedIn, (req, res) => {
+  res.send(`Welcome`);
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile', 'https://www.googleapis.com/auth/calendar'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/success');
+  }
+);
+
+app.get("/logout", (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(PORT, () => console.log('SERVER RUNNING AT PORT 3000'));
+
---
+++ google-auth.js
@@ -0,0 +1,26 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require('passport');
+require('dotenv').config();
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+    passReqToCallback: true
+  },
+  function(request, accessToken, refreshToken, profile, cb) {
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+    console.log('ACCESS TOKEN -> ', accessToken);
+    return cb(null, profile);
+  }
+));
